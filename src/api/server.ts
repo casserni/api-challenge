ada0782b0d7d6af7bed2fe73386dd599d85b9a58
casserni <nicholascassera@gmail.com>
2021-03-31 20:34:52 -0400

    feat: env example

--- src/api/server.ts
+++ src/api/server.ts
@@ -13,11 +13,7 @@ dotenv.config();
  * App Variables
  */
 
-if (!process.env.PORT) {
-  process.exit(1);
-}
-
-const PORT: number = parseInt(process.env.PORT as string, 10);
+const PORT: number = parseInt(process.env.PORT as string, 10) || 7000;
 
 const app = express();
 
